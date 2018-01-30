b1b25dbec33266fca2768fdc33dff5208bc9e3a4
Carolina Calvo <pececillu@gmail.com>
2018-01-30 10:50:56 +0100

    Arregar warning de versión de Kotlin y nota para ejecutar los tests

--- repository/src/androidTest/java/com/calvo/carolina/repository/ShopDAOTests.kt
+++ repository/src/androidTest/java/com/calvo/carolina/repository/ShopDAOTests.kt
@@ -1,6 +1,5 @@
 package com.calvo.carolina.repository
 
-import android.content.Context
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
 import com.calvo.carolina.repository.db.buildDBHelper
@@ -13,12 +12,16 @@ import org.junit.runner.RunWith
 import org.junit.Assert.*
 
 /**
+ * ----------------------------------------------------------------------------------
+ *   OJO CUIDAO!! Para que funcionen los tests hay que crear una congiguración de RUN
+ *   Run -> Edit Configurations -> + -> Android Instrumented Tests
+ * ----------------------------------------------------------------------------------
  * Instrumented test, which will execute on an Android device.
  *
  * @see [Testing documentation](http://d.android.com/tools/testing)
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest
+class ShopDAOTests
 {
     val appContext = InstrumentationRegistry.getTargetContext()
     val dbHelper = buildDBHelper(appContext, "mydb.sqlite", 1)
@@ -35,6 +38,6 @@ class ExampleInstrumentedTest
         val id = shopDAO.insert(shop)
 
         // Assert
-        assertTrue("El id tiene que ser mayor que ", id > 0)
+        assertTrue("El id tiene que ser mayor que 0", id > 0)
     }
 }
